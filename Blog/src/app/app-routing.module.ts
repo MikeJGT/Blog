d0791aaf5d92f0334f251fb463f13f3d2d727139
miky3000 <mikecode808@gmail.com>
2023-03-16 21:54:00 +0100

    Routing: Definicion de las diferentes rutas del proyecto

---
+++ Blog/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/posts' },
+  { path: 'posts', component: ListaPostsComponent },
+  { path: 'posts/new', component: FormularioComponent },
+  { path: '**', redirectTo: '/posts' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
