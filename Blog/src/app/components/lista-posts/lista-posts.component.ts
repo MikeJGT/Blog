7c82edd183eb833f8bab8a6493f4bbe18df0e4b5
miky3000 <mikecode808@gmail.com>
2023-03-17 23:14:56 +0100

    Incorporacion de estilos al componente Lista-posts

--- Blog/src/app/components/lista-posts/lista-posts.component.ts
+++ Blog/src/app/components/lista-posts/lista-posts.component.ts
@@ -10,10 +10,21 @@ import { PostsService } from 'src/app/services/posts.service';
 export class ListaPostsComponent {
 
   listaPosts: Post[]
+  prueba: Post
   constructor(private postSv: PostsService) {
-    this.listaPosts = []
+    this.listaPosts = [],
+      this.prueba = {
+        titulo: 'El Gato',
+        texto: 'Erase una vez un gato bonito  lsssssss  ssss sssssssssss  ssss sssss ssss ssssssss ssssssss ssssssss ssssssss sssssssssssssssss asnasdlaksdkamsdmlaskmdamsdma kasldmasmd lamsdm alsmdl maslkmlkam dlkamdlamsdlmalkmlk amslkamdlk maslkdm lkmdlk kmasm l',
+        autor: 'El gato con botas',
+        imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE6uHWJquRf_AwM8-mnbv2UeOtsd2BFFiXEg&usqp=CAU',
+        fecha: new Date("2023-03-25"),
+        categoria: 'Gatos'
+
+      }
   }
   ngOnInit() {
     this.listaPosts = this.postSv.getAll();
+    console.log('lista', this.listaPosts)
   }
 }
