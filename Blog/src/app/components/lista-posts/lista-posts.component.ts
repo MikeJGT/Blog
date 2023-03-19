686714cae6cd83dec7bc23353a12ca7e66a84013
miky3000 <mikecode808@gmail.com>
2023-03-19 22:55:00 +0100

    Correcion del componente lista-posts

--- Blog/src/app/components/lista-posts/lista-posts.component.ts
+++ Blog/src/app/components/lista-posts/lista-posts.component.ts
@@ -10,21 +10,28 @@ import { PostsService } from 'src/app/services/posts.service';
 export class ListaPostsComponent {
 
   listaPosts: Post[]
-  prueba: Post
+  categorias: string[]
+  //prueba: Post
   constructor(private postSv: PostsService) {
     this.listaPosts = [],
-      this.prueba = {
-        titulo: 'El Gato',
-        texto: 'Erase una vez un gato bonito  lsssssss  ssss sssssssssss  ssss sssss ssss ssssssss ssssssss ssssssss ssssssss sssssssssssssssss asnasdlaksdkamsdmlaskmdamsdma kasldmasmd lamsdm alsmdl maslkmlkam dlkamdlamsdlmalkmlk amslkamdlk maslkdm lkmdlk kmasm l',
-        autor: 'El gato con botas',
-        imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE6uHWJquRf_AwM8-mnbv2UeOtsd2BFFiXEg&usqp=CAU',
-        fecha: new Date("2023-03-25"),
-        categoria: 'Gatos'
-
-      }
+      this.categorias = []
+    // this.prueba = {
+    //   titulo: 'El Gato',
+    //   texto: 'Erase una vez un gato bonito  lsssssss  ssss sssssssssss  ssss sssss ssss ssssssss ssssssss ssssssss ssssssss sssssssssssssssss asnasdlaksdkamsdmlaskmdamsdma kasldmasmd lamsdm alsmdl maslkmlkam dlkamdlamsdlmalkmlk amslkamdlk maslkdm lkmdlk kmasm l',
+    //   autor: 'El gato con botas',
+    //   imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE6uHWJquRf_AwM8-mnbv2UeOtsd2BFFiXEg&usqp=CAU',
+    //   fecha: new Date("2023-03-25"),
+    //   categoria: 'Gatos'
+    // }
   }
   ngOnInit() {
     this.listaPosts = this.postSv.getAll();
+    this.categorias = this.postSv.getAllCategories()
     console.log('lista', this.listaPosts)
+  }
+
+  byCategory(category: any) {
+    this.listaPosts = this.postSv.getByCategory(category.target.value)
+    console.log('Evento Categoria', category.target.value)
   }
 }
